a87ef27d5925b321f20fd67b643994c9e52ecb2f
Luiz Carlos Cavalcanti <ext-luiz-carlos.cavalcanti@nokia.com>
2013-10-14 19:22:50 -0400

    Alter Main class of preprocessor to use Utils
    Signed-off-by: Luiz Carlos Cavalcanti <ext-luiz-carlos.cavalcanti@nokia.com>

--- preprocessor/src/br/edu/ufam/icomp/ammd/Main.java
+++ preprocessor/src/br/edu/ufam/icomp/ammd/Main.java
@@ -8,7 +8,6 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Properties;
 
 import javax.imageio.ImageIO;
 
@@ -20,7 +19,7 @@ public class Main {
     private static int imageCount = 0;
 
     public static void main(String[] args) {
-        loadConfiguration();
+        config = Configuration.loadConfiguration();
         preprocessImages();
     }
 
@@ -72,77 +71,6 @@ public class Main {
         }
         long listEnd = System.currentTimeMillis();
         System.out.println("Image list composed in " + (listEnd - listStart) + "ms");
-    }
-
-    private static void loadConfiguration() {
-        try {
-            Properties prop = loadConfigFile();
-            setupProperties(prop);
-            displayConfiguration();
-        } catch (IOException e) {
-            System.err.println("Cound not load configuration file: " + e.getMessage());
-            System.exit(-1);
-        }
-    }
-
-    private static Properties loadConfigFile() throws IOException {
-        Properties prop = new Properties();
-        FileInputStream fis = new FileInputStream("config.properties");
-        prop.load(fis);
-        return prop;
-    }
-
-    private static void setupProperties(Properties prop) {
-        validateProperties(prop);
-        config = new Configuration();
-        config.setRawDataDirectory(prop.getProperty("path.raw"));
-        config.setProcessedDataDirectory(prop.getProperty("path.processed"));
-        config.setChunkWidth(Integer.parseInt(prop.getProperty("chunk.width")));
-        config.setChunkHeight(Integer.parseInt(prop.getProperty("chunk.height")));
-    }
-
-    private static void validateProperties(Properties prop) {
-        if (prop.getProperty("path.raw") == null) {
-            System.err.println("configuration path.raw is obligatory");
-            System.exit(-1);
-        }
-        if (prop.getProperty("path.processed") == null) {
-            System.err.println("configuration path.processed is obligatory");
-            System.exit(-1);
-        }
-        if (prop.getProperty("chunk.width") == null) {
-            System.err.println("configuration chunk.width is obligatory");
-            System.exit(-1);
-        }
-        if (prop.getProperty("chunk.height") == null) {
-            System.err.println("configuration chunk.height is obligatory");
-            System.exit(-1);
-        }
-    }
-
-    private static void displayConfiguration() {
-        System.out.println("[Configuration loaded]");
-        System.out.println("Raw data directory: " + config.getRawDataDirectory());
-        System.out.println("Processed data directory: " + config.getProcessedDataDirectory());
-    }
-
-    private static Color getAverageColor(BufferedImage bitmap) {
-        long redBucket = 0;
-        long greenBucket = 0;
-        long blueBucket = 0;
-        long pixelCount = 0;
-        // byte[] pixels = ((DataBufferByte) bitmap.getRaster().getDataBuffer()).getData();
-        for (int y = 0; y < bitmap.getHeight(); y++) {
-            for (int x = 0; x < bitmap.getWidth(); x++) {
-                Color c = new Color(bitmap.getRGB(x, y));
-                pixelCount++;
-                redBucket += c.getRed();
-                greenBucket += c.getGreen();
-                blueBucket += c.getBlue();
-                // does alpha matter?
-            }
-        }
-        return new Color((int)(redBucket/pixelCount), (int)(greenBucket/pixelCount), (int)(blueBucket/pixelCount));
     }
 
 }
