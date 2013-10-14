bcb932c77a752ed82dcf2489b9bc61c930f3685a
Luiz Carlos Cavalcanti <ext-luiz-carlos.cavalcanti@nokia.com>
2013-10-14 19:20:34 -0400

    Refactor on Configuration class
    Signed-off-by: Luiz Carlos Cavalcanti <ext-luiz-carlos.cavalcanti@nokia.com>

--- preprocessor/src/br/edu/ufam/icomp/ammd/Configuration.java
+++ preprocessor/src/br/edu/ufam/icomp/ammd/Configuration.java
@@ -1,11 +1,65 @@
 package br.edu.ufam.icomp.ammd;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
 public class Configuration {
 
     private String rawDataDirectory;
     private String processedDataDirectory;
     private int chunkWidth;
     private int chunkHeight;
+
+    private Configuration() {
+    }
+
+    public static Configuration loadConfiguration() {
+        try {
+            Properties prop = loadConfigFile();
+            return setupProperties(prop);
+        } catch (IOException e) {
+            System.err.println("Cound not load configuration file: " + e.getMessage());
+            System.exit(-1);
+        }
+        return null;
+    }
+
+    private static Properties loadConfigFile() throws IOException {
+        Properties prop = new Properties();
+        FileInputStream fis = new FileInputStream("config.properties");
+        prop.load(fis);
+        return prop;
+    }
+
+    private static Configuration setupProperties(Properties prop) {
+        validateProperties(prop);
+        Configuration config = new Configuration();
+        config.setRawDataDirectory(prop.getProperty("path.raw"));
+        config.setProcessedDataDirectory(prop.getProperty("path.processed"));
+        config.setChunkWidth(Integer.parseInt(prop.getProperty("chunk.width")));
+        config.setChunkHeight(Integer.parseInt(prop.getProperty("chunk.height")));
+        return config;
+    }
+
+    private static void validateProperties(Properties prop) {
+        if (prop.getProperty("path.raw") == null) {
+            System.err.println("configuration path.raw is obligatory");
+            System.exit(-1);
+        }
+        if (prop.getProperty("path.processed") == null) {
+            System.err.println("configuration path.processed is obligatory");
+            System.exit(-1);
+        }
+        if (prop.getProperty("chunk.width") == null) {
+            System.err.println("configuration chunk.width is obligatory");
+            System.exit(-1);
+        }
+        if (prop.getProperty("chunk.height") == null) {
+            System.err.println("configuration chunk.height is obligatory");
+            System.exit(-1);
+        }
+    }
 
     public String getRawDataDirectory() {
         return rawDataDirectory;
