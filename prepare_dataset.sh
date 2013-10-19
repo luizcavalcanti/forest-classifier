c16b50096140173cfd54d98cbff93f067b918c7c
Luiz Carlos Cavalcanti <ext-luiz-carlos.cavalcanti@nokia.com>
2013-10-19 19:21:42 -0400

    Improving shell scripts
    Signed-off-by: Luiz Carlos Cavalcanti <ext-luiz-carlos.cavalcanti@nokia.com>

--- prepare_dataset.sh
+++ prepare_dataset.sh
@@ -8,3 +8,12 @@ java -cp forestclassifier.jar br.edu.ufam.icomp.ammd.ImagePreprocessor
 
 # call GUI for dataset classification tool
 java -cp forestclassifier.jar br.edu.ufam.icomp.ammd.VisualClassifier
+
+# remove non-numeric parameters
+java -cp lib/weka.jar weka.filters.unsupervised.attribute.Remove -R 1,2 -i forest.arff -o forest_.arff
+mv forest_.arff forest.arff
+
+# once the whole dataset is classified,
+# we should split it into training and test datasets
+java -cp lib/weka.jar weka.filters.unsupervised.instance.RemovePercentage -P 40 -i forest.arff  -o  forest_train.arff
+java -cp lib/weka.jar weka.filters.unsupervised.instance.RemovePercentage -P 60 -V -i forest.arff  -o  forest_test.arff
--- run_experiment.sh
+++ run_experiment.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
-# check database
-java -cp lib/weka.jar weka.core.Instances forest.arff
 
-# remove non-numeric parameters
-java -cp lib/weka.jar weka.filters.unsupervised.attribute.Remove -R 1,2 -i forest.arff -o forest_train.arff
+# prepare log files
+rm -rf log/
+mkdir log
+
+# check database
+java -cp lib/weka.jar weka.core.Instances forest_train.arff
+java -cp lib/weka.jar weka.core.Instances forest_test.arff
 
 # classify with Decision Tree (J48)
-java -cp lib/weka.jar weka.classifiers.trees.J48 -t forest_train.arff
+ # 2>&1 | tee -a example.txt
+java -cp lib/weka.jar weka.classifiers.trees.J48 -t forest_train.arff -T forest_test.arff > log/decision_tree.log
 
 # classify with kNN (IBk)
-java -cp lib/weka.jar weka.classifiers.lazy.IBk -K 1 -t forest_train.arff
-java -cp lib/weka.jar weka.classifiers.lazy.IBk -K 2 -t forest_train.arff
+java -cp lib/weka.jar weka.classifiers.lazy.IBk -K 1 -t forest_train.arff -T forest_test.arff > log/knn_1.log
+java -cp lib/weka.jar weka.classifiers.lazy.IBk -K 2 -t forest_train.arff -T forest_test.arff > log/knn_2.log
 
 # classify with SVM
-java -cp lib/weka.jar:lib/libsvm.jar weka.classifiers.functions.LibSVM -t forest_train.arff
+java -cp lib/weka.jar:lib/libsvm.jar weka.classifiers.functions.LibSVM -t forest_train.arff -T forest_test.arff > log/svm.log
