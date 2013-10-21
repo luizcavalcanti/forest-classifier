a33d9b06eace6bc9cad624b6f1fab53b2043f6c8
Luiz Carlos Cavalcanti <ext-luiz-carlos.cavalcanti@nokia.com>
2013-10-20 22:08:24 -0400

    Correction on kNN parameters
    Signed-off-by: Luiz Carlos Cavalcanti <ext-luiz-carlos.cavalcanti@nokia.com>

--- run_experiment.sh
+++ run_experiment.sh
@@ -14,7 +14,7 @@ java -cp lib/weka.jar weka.classifiers.trees.J48 -t forest_train.arff -T forest_
 
 # classify with kNN (IBk)
 java -cp lib/weka.jar weka.classifiers.lazy.IBk -K 1 -t forest_train.arff -T forest_test.arff > log/knn_1.log
-java -cp lib/weka.jar weka.classifiers.lazy.IBk -K 2 -t forest_train.arff -T forest_test.arff > log/knn_2.log
+java -cp lib/weka.jar weka.classifiers.lazy.IBk -K 3 -t forest_train.arff -T forest_test.arff > log/knn_3.log
 
 # classify with SVM
 java -cp lib/weka.jar:lib/libsvm.jar weka.classifiers.functions.LibSVM -t forest_train.arff -T forest_test.arff > log/svm.log
