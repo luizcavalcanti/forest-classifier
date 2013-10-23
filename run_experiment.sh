2feaa6b850dc7785df3d0d4c39e086a19a261934
Luiz Carlos Cavalcanti <ext-luiz-carlos.cavalcanti@nokia.com>
2013-10-23 03:07:31 -0400

    Proper separation of training and test bases
    Signed-off-by: Luiz Carlos Cavalcanti <ext-luiz-carlos.cavalcanti@nokia.com>

--- run_experiment.sh
+++ run_experiment.sh
@@ -9,12 +9,13 @@ java -cp lib/weka.jar weka.core.Instances forest_train.arff
 java -cp lib/weka.jar weka.core.Instances forest_test.arff
 
 # classify with Decision Tree (J48)
- # 2>&1 | tee -a example.txt
-java -cp lib/weka.jar weka.classifiers.trees.J48 -t forest_train.arff -T forest_test.arff > log/decision_tree.log
+java -cp lib/weka.jar weka.classifiers.trees.J48 -C 0.5  -t forest_train.arff -d j48.model > log/decision_tree_train.log
+java -cp lib/weka.jar weka.classifiers.trees.J48 -T forest_test.arff -l j48.model > log/decision_tree_test.log
 
 # classify with kNN (IBk)
-java -cp lib/weka.jar weka.classifiers.lazy.IBk -K 1 -t forest_train.arff -T forest_test.arff > log/knn_1.log
-java -cp lib/weka.jar weka.classifiers.lazy.IBk -K 3 -t forest_train.arff -T forest_test.arff > log/knn_3.log
+java -cp lib/weka.jar weka.classifiers.lazy.IBk -K 5 -t forest_train.arff -d knn.model > log/knn_train.log
+java -cp lib/weka.jar weka.classifiers.lazy.IBk -T forest_test.arff -l knn.model > log/knn_test.log
 
 # classify with SVM
-java -cp lib/weka.jar:lib/libsvm.jar weka.classifiers.functions.LibSVM -K 2 -G 0.021 -D 1 -E 0.00001 -t forest_train.arff -T forest_test.arff >> log/svm.log
+java -cp lib/weka.jar:lib/libsvm.jar weka.classifiers.functions.LibSVM -K 2 -G 0.021 -D 1 -E 0.00001 -t forest_train.arff -d svm.model > log/svm_train.log
+java -cp lib/weka.jar:lib/libsvm.jar weka.classifiers.functions.LibSVM -T forest_test.arff -l svm.model > log/svm_test.log
