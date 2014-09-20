ea3ac9324d4f3652c485c6da00fd0adf0fdc9fda
Luiz Carlos Cavalcanti <cavalcanti.luiz@gmail.com>
2014-09-19 21:30:19 -0400

    Added more universal script (compatible with zsh and stuff)
    Signed-off-by: Luiz Carlos Cavalcanti <cavalcanti.luiz@gmail.com>

--- run_experiment.sh
+++ run_experiment.sh
@@ -5,17 +5,16 @@ rm -rf log/
 mkdir log
 
 # check database
-java -cp lib/weka.jar weka.core.Instances forest_train.arff
-java -cp lib/weka.jar weka.core.Instances forest_test.arff
+java -cp ".;lib/weka.jar" weka.core.Instances forest_train.arff
+java -cp ".;lib/weka.jar" weka.core.Instances forest_test.arff
 
 # classify with Decision Tree (J48)
-java -cp lib/weka.jar weka.classifiers.trees.J48 -C 0.5  -t forest_train.arff -d j48.model > log/decision_tree_train.log
-java -cp lib/weka.jar weka.classifiers.trees.J48 -T forest_test.arff -l j48.model > log/decision_tree_test.log
+java -cp ".;lib/weka.jar" weka.classifiers.trees.J48 -C 0.5  -t forest_train.arff -d j48.model > log/decision_tree_train.log
+java -cp ".;lib/weka.jar" weka.classifiers.trees.J48 -T forest_test.arff -l j48.model > log/decision_tree_test.log
 
 # classify with kNN (IBk)
-java -cp lib/weka.jar weka.classifiers.lazy.IBk -K 5 -t forest_train.arff -d knn.model > log/knn_train.log
-java -cp lib/weka.jar weka.classifiers.lazy.IBk -T forest_test.arff -l knn.model > log/knn_test.log
+java -cp ".;lib/weka.jar" weka.classifiers.lazy.IBk -K 5 -t forest_train.arff -d knn.model > log/knn_train.log
+java -cp ".;lib/weka.jar" weka.classifiers.lazy.IBk -T forest_test.arff -l knn.model > log/knn_test.log
 
 # classify with SVM
-java -cp lib/weka.jar:lib/libsvm.jar weka.classifiers.functions.LibSVM -K 2 -G 0.021 -D 1 -E 0.00001 -t forest_train.arff -d svm.model > log/svm_train.log
-java -cp lib/weka.jar:lib/libsvm.jar weka.classifiers.functions.LibSVM -T forest_test.arff -l svm.model > log/svm_test.log
+java -cp ".;lib/weka.jar;lib/libsvm.jar" weka.classifiers.functions.LibSVM -T forest_test.arff -l svm.model > log/svm_test.log
--- run_segmentation_experiment.sh
+++ run_segmentation_experiment.sh
@@ -2,6 +2,6 @@
 
 # Run learning-based segmentaiton experiment
 echo Running segmentation. This may take a while \(no kidding\)...
-java -cp forestclassifier.jar:lib/* br.edu.ufam.icomp.ammd.ImageSegmentationExperiment
+java -cp ".;forestclassifier.jar;lib/*" br.edu.ufam.icomp.ammd.ImageSegmentationExperiment
 echo Running overlay on output
-java -cp forestclassifier.jar:lib/* br.edu.ufam.icomp.ammd.SegmentationOverlay
+java -cp ".;forestclassifier.jar;lib/*" br.edu.ufam.icomp.ammd.SegmentationOverlay
