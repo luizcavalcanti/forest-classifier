08dbfc3ad540ad4c06304d3c6628b7309b9a0887
Luiz Carlos Cavalcanti <luiz.cavalcanti@indt.org.br>
2015-03-03 18:32:08 -0400

    creating better structure for segmentation experiments
    Signed-off-by: Luiz Carlos Cavalcanti <luiz.cavalcanti@indt.org.br>

--- run_segmentation_experiment.sh
+++ run_segmentation_experiment.sh
@@ -1,7 +1,45 @@
 #!/bin/bash
 
-# Run learning-based segmentaiton experiment
-echo Running segmentation. This may take a while \(no kidding\)...
-java -cp ".;forestclassifier.jar;lib/*" br.edu.ufam.icomp.ammd.ImageSegmentationExperiment
-echo Running overlay on output
-java -cp ".;forestclassifier.jar;lib/*" br.edu.ufam.icomp.ammd.SegmentationOverlay
+
+RUN_MEANSHIFT=true
+RUN_SRM=true
+RUN_LEARNING=true
+
+
+if [ "$RUN_MEANSHIFT" = true ] ; then
+    # Run meanshif
+    echo Running meanshift experiment...
+    STARTTIME=$(date +%s)
+    cd meanshift
+    ./meanshift.sh
+    cd ..
+    ENDTIME=$(date +%s)
+    echo "done in $(($ENDTIME - $STARTTIME)) seconds."
+    echo =============================================
+fi
+
+
+if [ "$RUN_SRM" = true ] ; then
+    # Run SRM
+    echo Running SRM experiment...
+    STARTTIME=$(date +%s)
+    cd srm
+    ./srm.sh
+    cd ..
+    ENDTIME=$(date +%s)
+    echo "done in $(($ENDTIME - $STARTTIME)) seconds."
+    echo =============================================
+fi
+
+
+if [ "$RUN_LEARNING" = true ] ; then
+    # Run learning-based segmentaiton experiment
+    echo Running learning-based experiment. This may take a while \(no kidding\)...
+    STARTTIME=$(date +%s)
+    java -cp ".:forestclassifier.jar:lib/*" br.edu.ufam.icomp.ammd.ImageSegmentationExperiment
+    echo Running overlay on output
+    java -cp ".:forestclassifier.jar:lib/*" br.edu.ufam.icomp.ammd.SegmentationOverlay
+    ENDTIME=$(date +%s)
+    echo "done in $(($ENDTIME - $STARTTIME)) seconds."
+    echo =============================================
+fi
--- srm/srm.sh
+++ srm/srm.sh
@@ -4,8 +4,8 @@ MATLAB_HOME=~/MATLAB/R2013b/
 MATLAB=$MATLAB_HOME/bin/matlab
 MEX=$MATLAB_HOME/bin/mex
 
-rm -Rf output
-mkdir output
+rm -Rf out
+mkdir out
 
 echo "compiling mex"
 $MEX srm_boundarygradient.c
