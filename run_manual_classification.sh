72bd601ce73a4809e3eb536c1a7de116596a7f4d
Luiz Carlos Cavalcanti <cavalcanti.luiz@gmail.com>
2015-06-10 00:24:35 -0400

    Simplify manual classification tool script
    Signed-off-by: Luiz Carlos Cavalcanti <cavalcanti.luiz@gmail.com>

--- run_manual_classification.sh
+++ run_manual_classification.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+if [ $# -eq 0 ]; then
+    echo "No arguments. Please provide the image database location as argument"
+    exit 1
+fi
+
+IMAGES_DIR=$1
+
 # go to subproject dir
 cd manual-classification
 
@@ -7,4 +14,4 @@ cd manual-classification
 ant build
 
 # call GUI for dataset classification tool
-java -cp dist/forestclassifier.jar br.edu.ufam.icomp.ammd.VisualClassifier
+java -cp dist/forestclassifier.jar br.edu.ufam.icomp.ammd.VisualClassifier "../$IMAGES_DIR"
