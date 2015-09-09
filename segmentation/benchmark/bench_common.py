64d7cdb56636cbc27c5f30a17369f9248d04360f
Luiz Carlos Cavalcanti <cavalcanti.luiz@gmail.com>
2015-09-09 07:33:21 -0400

    [segbench] new error calculation based on new dat files
    Signed-off-by: Luiz Carlos Cavalcanti <cavalcanti.luiz@gmail.com>

--- segmentation/benchmark/bench_common.py
+++ segmentation/benchmark/bench_common.py
@@ -2,14 +2,33 @@ import cv2
 import numpy as np
 
 def convert_dat_to_image(dat):
-    lines = dat.split('|')
-    width = len(lines)
-    height = len(lines[0])
+    lines = dat.split('\n')
+    regions = int(lines[0])
+    print str(regions) + " regions"
+    width = 640
+    height = 480
     image = np.zeros((height, width, 1), np.uint8)
-    for w in xrange(0, width):
-        for h in xrange(0,height):
-            image[h,w] = ord(lines[w][h])
+
+    for i in range(1,regions+1):
+        index = (2*i)
+        region_type = lines[index-1]
+        data = lines[index].replace('[','')
+        data = data.replace(']', ' ')
+        coords = data.split(' ')
+        if region_type == 'cr': # Closed region
+            start = coords[0].split(',')
+            end = coords[len(coords)-2].split(',')
+            draw_region_on_image(image, coords)
+            cv2.line(image, (int(end[0]), int(end[1])), (int(start[0]), int(start[1])), (255,255,255),1)
+        elif region_type == 'pt': # Path/Stroke
+            draw_region_on_image(image, coords)
     return image
+
+def draw_region_on_image(image, coords):
+    for c in range(0, len(coords)-2):
+        c1 = coords[c].split(',')
+        c2 = coords[c+1].split(',')
+        cv2.line(image, (int(c1[0]), int(c1[1])), (int(c2[0]), int(c2[1])), (255,255,255),1)
 
 def find_edges(image):
     width, height, depth = image.shape
