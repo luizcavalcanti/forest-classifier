d0c29c0ea476c11cac11c5900139064dee6a8856
Luiz Carlos Cavalcanti <cavalcanti.luiz@gmail.com>
2015-09-12 09:19:04 -0400

    [benchmark] log all output in logs folder
    Signed-off-by: Luiz Carlos Cavalcanti <cavalcanti.luiz@gmail.com>

--- run_segmentation_benchmark.sh
+++ run_segmentation_benchmark.sh
@@ -13,76 +13,71 @@ RUN_LEARNING=false
 
 MATLAB_HOME=$1
 IMAGES_DIR=$2
+LOG_DIR=logs
+RESULTS_DIR=results
+
+rm -rf $LOG_DIR
+mkdir $LOG_DIR
+rm -rf $RESULTS_DIR
+mkdir $RESULTS_DIR
 
 if [ "$RUN_MSEG" = true ] ; then
     # Run MSEG
-    echo Running MSEG experiment...
+    echo "Running MSEG experiment..."
     STARTTIME=$(date +%s)
     cd segmentation/mseg
-    ./mseg.sh "../../$IMAGES_DIR"
+    ./mseg.sh "../../$IMAGES_DIR" > "../../$LOG_DIR"/mseg.log
     cd ../..
     ENDTIME=$(date +%s)
-    echo "done in $(($ENDTIME - $STARTTIME)) seconds."
-    echo
-    echo =============================================
     echo "Running benchmark..."
-    python segmentation/benchmark/mseg_benchmark.py "$IMAGES_DIR" > benchmark_mseg.log
-    echo "done."
-    echo =============================================
+    python segmentation/benchmark/mseg_benchmark.py "$IMAGES_DIR" > "$RESULTS_DIR"/benchmark_mseg.log
+    echo "done"
+    echo "took $(($ENDTIME - $STARTTIME)) seconds"
     echo
 fi
 
 if [ "$RUN_JSEG" = true ] ; then
     # Run JSEG
-    echo Running JSEG experiment...
+    echo "Running JSEG experiment..."
     STARTTIME=$(date +%s)
     cd segmentation/jseg
-    ./jseg.sh "../../$IMAGES_DIR"
+    ./jseg.sh "../../$IMAGES_DIR" > "../../$LOG_DIR"/jseg.log
     cd ../..
     ENDTIME=$(date +%s)
-    echo "done in $(($ENDTIME - $STARTTIME)) seconds."
-    echo
-    echo =============================================
     echo "Running benchmark..."
-    python segmentation/benchmark/jseg_benchmark.py "$IMAGES_DIR" > benchmark_jseg.log
-    echo "done."
-    echo =============================================
+    python segmentation/benchmark/jseg_benchmark.py "$IMAGES_DIR" > "$RESULTS_DIR"/benchmark_jseg.log
+    echo "done"
+    echo "took $(($ENDTIME - $STARTTIME)) seconds"
     echo
 fi
 
 if [ "$RUN_MEANSHIFT" = true ] ; then
     # Run meanshift
-    echo Running meanshift experiment...
+    echo "Running meanshift experiment..."
     STARTTIME=$(date +%s)
     cd segmentation/meanshift
-    ./meanshift.sh "../../$IMAGES_DIR"
+    ./meanshift.sh "../../$IMAGES_DIR" > "../../$LOG_DIR"/meanshift.log
     cd ../..
     ENDTIME=$(date +%s)
-    echo "done in $(($ENDTIME - $STARTTIME)) seconds."
-    echo
-    echo =============================================
     echo "Running benchmark..."
-    python segmentation/benchmark/meanshift_benchmark.py "$IMAGES_DIR" > benchmark_meanshift.log
-    echo "done."
-    echo =============================================
+    python segmentation/benchmark/meanshift_benchmark.py "$IMAGES_DIR" > "$RESULTS_DIR"/benchmark_meanshift.log
+    echo "done"
+    echo "took $(($ENDTIME - $STARTTIME)) seconds"
     echo
 fi
 
 if [ "$RUN_SRM" = true ] ; then
     # Run SRM
-    echo Running SRM experiment...
+    echo "Running SRM experiment..."
     STARTTIME=$(date +%s)
     cd segmentation/srm
-    ./srm.sh "$MATLAB_HOME" "../../$IMAGES_DIR"
+    ./srm.sh "$MATLAB_HOME" "../../$IMAGES_DIR" > "../../$LOG_DIR"/srm.log
     cd ../..
     ENDTIME=$(date +%s)
-    echo "done in $(($ENDTIME - $STARTTIME)) seconds."
-    echo
-    echo =============================================
     echo "Running benchmark..."
-    python segmentation/benchmark/srm_benchmark.py "$IMAGES_DIR" > benchmark_srm.log
-    echo "done."
-    echo =============================================
+    python segmentation/benchmark/srm_benchmark.py "$IMAGES_DIR" > "$RESULTS_DIR"/benchmark_srm.log
+    echo "done"
+    echo "took $(($ENDTIME - $STARTTIME)) seconds"
     echo
 fi
 
@@ -96,6 +91,7 @@ if [ "$RUN_LEARNING" = true ] ; then
     echo Running overlay on output
     java -cp ".:learning-segmentation.jar:../../lib/*" br.edu.ufam.icomp.ammd.SegmentationOverlay
     ENDTIME=$(date +%s)
-    echo "done in $(($ENDTIME - $STARTTIME)) seconds."
-    echo =============================================
+    echo "done"
+    echo "took $(($ENDTIME - $STARTTIME)) seconds"
+    echo
 fi
