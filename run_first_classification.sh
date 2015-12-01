f5a1bbd61fb47330a731bb18526d5042fd594e26
Luiz Cavalcanti <luiz.cavalcanti@indt.org.br>
2015-12-01 14:34:35 -0400

    [first] improve execution script
    Signed-off-by: Luiz Cavalcanti <luiz.cavalcanti@indt.org.br>

--- run_first_classification.sh
+++ run_first_classification.sh
@@ -5,7 +5,10 @@ TEMP_PATH=tmp
 # Uncomment if need to run segmentation all over again
 # ./srm.sh "$MATLAB_HOME" "../../$IMAGES_DIR"
 
-# Uncommend if need to generate samples for labeling
+# Uncomment if need to generate samples for labeling
+# rm -rf "$TEMP_PATH"
+# mkdir "$TEMP_PATH"
 # python first-classification/generate_samples.py "database/ptv-mao" "segmentation/srm/out/" "$TEMP_PATH"
 
+# Uncomment if need to generate initial arff file
 python first-classification/generate_arff_database.py "$TEMP_PATH" "first.arff"
