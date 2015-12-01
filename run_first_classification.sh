363c899a9e46ae795784a65466969ebd872389f8
Luiz Cavalcanti <luiz.cavalcanti@indt.org.br>
2015-12-01 17:08:37 -0400

    [first] improve script with proper params
    Signed-off-by: Luiz Cavalcanti <luiz.cavalcanti@indt.org.br>

--- run_first_classification.sh
+++ run_first_classification.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 TEMP_PATH=tmp
+ORIGINAL_IMAGES=database/ptv-mao
+SEGMENTED_IMAGES=segmentation/srm/out/
+ARFF_PATH=first.arff
 
 # Uncomment if need to run segmentation all over again
 # ./srm.sh "$MATLAB_HOME" "../../$IMAGES_DIR"
@@ -8,7 +11,9 @@ TEMP_PATH=tmp
 # Uncomment if need to generate samples for labeling
 # rm -rf "$TEMP_PATH"
 # mkdir "$TEMP_PATH"
-# python first-classification/generate_samples.py "database/ptv-mao" "segmentation/srm/out/" "$TEMP_PATH"
+# python first-classification/generate_samples.py "$ORIGINAL_IMAGES" "$SEGMENTED_IMAGES" "$TEMP_PATH"
 
 # Uncomment if need to generate initial arff file
-python first-classification/generate_arff_database.py "$TEMP_PATH" "first.arff"
+python first-classification/generate_arff_database.py "$TEMP_PATH" "$ARFF_PATH"
+
+# python first-classification/label_samples.py "$ORIGINAL_IMAGES" "$TEMP_PATH" "$ARFF_PATH"
