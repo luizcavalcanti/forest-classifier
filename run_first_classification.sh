ec3f73c0f0090690e441a0f09b6e04dc44e70996
Luiz Cavalcanti <luiz.cavalcanti@indt.org.br>
2015-12-03 17:18:50 -0400

    [first] added knn on experiment
    Signed-off-by: Luiz Cavalcanti <luiz.cavalcanti@indt.org.br>

--- run_first_classification.sh
+++ run_first_classification.sh
@@ -31,19 +31,30 @@ ARFF_PATH=first.arff
 FILTERED_ARFF=filtered_$ARFF_PATH
 TRAINING_ARFF=train_$ARFF_PATH
 TESTING_ARFF=test_$ARFF_PATH
+
 # remove useless attribute (sample id)
 java -cp ".:$WEKA_DIR/weka.jar" weka.filters.unsupervised.attribute.Remove -R 1 -i "$ARFF_PATH" -o "$FILTERED_ARFF"
+
 # split dataset into training and test datasets (66-33 split)
 java -cp ".:$WEKA_DIR/weka.jar" weka.filters.unsupervised.instance.RemovePercentage -P 34 -i "$FILTERED_ARFF"  -o "$TRAINING_ARFF"
 java -cp ".:$WEKA_DIR/weka.jar" weka.filters.unsupervised.instance.RemovePercentage -P 34 -i "$FILTERED_ARFF"  -o "$TESTING_ARFF" -V
+
 # print complete dataset stats
 # java -cp ".:$WEKA_DIR/weka.jar" weka.core.Instances "$FILTERED_ARFF"
+
 # print training dataset stats
 # java -cp ".:$WEKA_DIR/weka.jar" weka.core.Instances "$TRAINING_ARFF"
+
 # print testing dataset stats
 # java -cp ".:$WEKA_DIR/weka.jar" weka.core.Instances "$TESTING_ARFF"
+
 # run decision tree (J48)
 java -cp ".:$WEKA_DIR/weka.jar" weka.classifiers.trees.J48 -t "$TRAINING_ARFF" -T "$TESTING_ARFF" -d results/first-j48.model -C 0.3
+# run kNN (IBk)
+java -cp ".:$WEKA_DIR/weka.jar" weka.classifiers.lazy.IBk -t "$TRAINING_ARFF" -T "$TESTING_ARFF" -d results/first-knn-1.model -K 1
+java -cp ".:$WEKA_DIR/weka.jar" weka.classifiers.lazy.IBk -t "$TRAINING_ARFF" -T "$TESTING_ARFF" -d results/first-knn-2.model -K 2
+java -cp ".:$WEKA_DIR/weka.jar" weka.classifiers.lazy.IBk -t "$TRAINING_ARFF" -T "$TESTING_ARFF" -d results/first-knn-3.model -K 3
+
 # remove temporary datasets
 rm "$FILTERED_ARFF"
 rm "$TRAINING_ARFF"
