a25e849a13507f1ba673cf0490f793d56bd037f4
Luiz Carlos Cavalcanti <cavalcanti.luiz@gmail.com>
2016-01-16 21:07:17 -0400

    [first] generate output for cfs and non-cfs datasets
    Signed-off-by: Luiz Carlos Cavalcanti <cavalcanti.luiz@gmail.com>

--- run_first_classification.sh
+++ run_first_classification.sh
@@ -22,24 +22,27 @@ SPLIT_DATASET=false
 # python first-classification/label_samples.py "$ORIGINAL_IMAGES" "$SAMPLES_PATH" "$ARFF_PATH"
 
 # Uncomment if need to update arff attributes (it spares id and class only)
-python first-classification/generate_arff_attributes.py "$SAMPLES_PATH" "$ARFF_PATH"
+#python first-classification/generate_arff_attributes.py "$SAMPLES_PATH" "$ARFF_PATH"
 
 ####################################
 ## run classification experiments ##
 ####################################
 FILTERED_ARFF=filtered_$ARFF_PATH
+CFS_ARFF=cfs_$ARFF_PATH
 TRAINING_ARFF=train_$ARFF_PATH
 TESTING_ARFF=test_$ARFF_PATH
+TRAINING_CFS_ARFF=train_$ARFF_PATH
+TESTING_CFS_ARFF=test_$ARFF_PATH
 
 echo "remove useless attribute (sample id)"
 java -cp $WEKA_LIBS weka.filters.unsupervised.attribute.Remove -R 1 -i "$ARFF_PATH" -o "$FILTERED_ARFF"
 
 echo "executing attribute selection (CFS & BestFirst)"
-java -cp $WEKA_LIBS weka.filters.supervised.attribute.AttributeSelection -E "weka.attributeSelection.CfsSubsetEval -P 4 -E 4" -S "weka.attributeSelection.BestFirst -D 2 -N 5" -i "$FILTERED_ARFF" -o "tmp-$FILTERED_ARFF"
-mv "tmp-$FILTERED_ARFF" "$FILTERED_ARFF"
+java -cp $WEKA_LIBS weka.filters.supervised.attribute.AttributeSelection -E "weka.attributeSelection.CfsSubsetEval -P 4 -E 4" -S "weka.attributeSelection.BestFirst -D 2 -N 5" -i "$FILTERED_ARFF" -o "$CFS_ARFF"
 
 echo "running complete dataset analysis"
 java -cp $WEKA_LIBS weka.core.Instances "$FILTERED_ARFF"  > results/first-database-analysis.txt
+java -cp $WEKA_LIBS weka.core.Instances "$CFS_ARFF"  > results/first-database-cfs-analysis.txt
 
 # enabled training/test dataset split?
 if $SPLIT_DATASET; then
@@ -47,35 +50,52 @@ if $SPLIT_DATASET; then
     java -cp $WEKA_LIBS weka.filters.unsupervised.instance.RemovePercentage -P 34 -i "$FILTERED_ARFF"  -o "$TRAINING_ARFF"
     java -cp $WEKA_LIBS weka.filters.unsupervised.instance.RemovePercentage -P 34 -i "$FILTERED_ARFF"  -o "$TESTING_ARFF" -V
 
+    java -cp $WEKA_LIBS weka.filters.unsupervised.instance.RemovePercentage -P 34 -i "$CFS_ARFF"  -o "$TRAINING_CFS_ARFF"
+    java -cp $WEKA_LIBS weka.filters.unsupervised.instance.RemovePercentage -P 34 -i "$CFS_ARFF"  -o "$TESTING_CFS_ARFF" -V
+
     echo "running training dataset analysis"
-    java -cp $WEKA_LIBS weka.core.Instances "$TRAINING_ARFF" > results/first-training-database-analysis.txt
+    java -cp $WEKA_LIBS weka.core.Instances "$TRAINING_ARFF" > results/first-training-analysis.txt
+    java -cp $WEKA_LIBS weka.core.Instances "$TRAINING_CFS_ARFF" > results/first-training-cfs-analysis.txt
 
     echo "running testing dataset analysis"
-    java -cp $WEKA_LIBS weka.core.Instances "$TESTING_ARFF" > results/first-testing-database-analysis.txt
+    java -cp $WEKA_LIBS weka.core.Instances "$TESTING_ARFF" > results/first-testing-analysis.txt
+    java -cp $WEKA_LIBS weka.core.Instances "$TESTING_CFS_ARFF" > results/first-testing-cfs-analysis.txt
 
     ARFF_OPTIONS="-t $TRAINING_ARFF -T $TESTING_ARFF"
+    ARFF_OPTIONS_CFS="-t $TRAINING_CFS_ARFF -T $TESTING_CFS_ARFF"
 else
     ARFF_OPTIONS="-t $FILTERED_ARFF"
+    ARFF_OPTIONS_CFS="-t $CFS_ARFF"
 fi
 
 echo "running decision tree (J48)"
 java -cp $WEKA_LIBS weka.classifiers.trees.J48 $ARFF_OPTIONS -C 0.3 -i -d results/first-j48.model > results/first-j48-results.txt
+java -cp $WEKA_LIBS weka.classifiers.trees.J48 $ARFF_OPTIONS_CFS -C 0.3 -i -d results/first-j48-cfs.model > results/first-j48-cfs-results.txt
 
 echo "running random forest"
 java -cp $WEKA_LIBS weka.classifiers.trees.RandomForest $ARFF_OPTIONS -K 10 -I 200 -i -d results/first-randomforest.model > results/first-randomforest-results.txt
+java -cp $WEKA_LIBS weka.classifiers.trees.RandomForest $ARFF_OPTIONS_CFS -K 10 -I 200 -i -d results/first-randomforest-cfs.model > results/first-randomforest-cfs-results.txt
 
 echo "running kNN (IBk)"
 java -cp $WEKA_LIBS weka.classifiers.lazy.IBk $ARFF_OPTIONS -K 1 -i -d results/first-knn-1.model > results/first-knn1-results.txt
 java -cp $WEKA_LIBS weka.classifiers.lazy.IBk $ARFF_OPTIONS -K 2 -i -d results/first-knn-2.model > results/first-knn2-results.txt
 java -cp $WEKA_LIBS weka.classifiers.lazy.IBk $ARFF_OPTIONS -K 3 -i -d results/first-knn-3.model > results/first-knn3-results.txt
+java -cp $WEKA_LIBS weka.classifiers.lazy.IBk $ARFF_OPTIONS_CFS -K 1 -i -d results/first-knn-1-cfs.model > results/first-knn1-cfs-results.txt
+java -cp $WEKA_LIBS weka.classifiers.lazy.IBk $ARFF_OPTIONS_CFS -K 2 -i -d results/first-knn-2-cfs.model > results/first-knn2-cfs-results.txt
+java -cp $WEKA_LIBS weka.classifiers.lazy.IBk $ARFF_OPTIONS_CFS -K 3 -i -d results/first-knn-3-cfs.model > results/first-knn3-cfs-results.txt
 
 echo "running SVM"
 java -cp $WEKA_LIBS weka.classifiers.functions.LibSVM $ARFF_OPTIONS -i -d results/first-svm.model > results/first-svm-results.txt
+java -cp $WEKA_LIBS weka.classifiers.functions.LibSVM $ARFF_OPTIONS -i -d results/first-svm-cfs.model > results/first-svm-cfs-results.txt
 
 echo "running naive bayes"
 java -cp $WEKA_LIBS weka.classifiers.bayes.NaiveBayes $ARFF_OPTIONS -i -d results/first-naive.model > results/first-naive-results.txt
+java -cp $WEKA_LIBS weka.classifiers.bayes.NaiveBayes $ARFF_OPTIONS -i -d results/first-naive-cfs.model > results/first-naive-cfs-results.txt
 
 echo "removing temporary datasets"
 rm "$FILTERED_ARFF" 2> /dev/null
+rm "$CFS_ARFF" 2> /dev/null
 rm "$TRAINING_ARFF" 2> /dev/null
 rm "$TESTING_ARFF" 2> /dev/null
+rm "$TRAINING_CFS_ARFF" 2> /dev/null
+rm "$TESTING_CFS_ARFF" 2> /dev/null
