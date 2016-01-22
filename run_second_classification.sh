126eec5cb2f46665dfa6e25ec9072f71f3b73a15
Luiz Carlos Cavalcanti <cavalcanti.luiz@gmail.com>
2016-01-21 23:16:54 -0400

    [second] reclassifying data with winner classifier
    Signed-off-by: Luiz Carlos Cavalcanti <cavalcanti.luiz@gmail.com>

--- run_second_classification.sh
+++ run_second_classification.sh
@@ -3,6 +3,15 @@
 WEKA_LIBS=".:manual-classification/lib/*"
 ORIGINAL_IMAGES=database/ptv-mao
 SEGMENTED_IMAGES=segmentation/srm/out/
-ARFF_PATH=first.arff
 
-python second-classification/separate_arff_by_class.py "$ARFF_PATH"
+# first classifier config
+FIRST_ARFF=first.arff
+FIRST_FILTERS="weka.filters.unsupervised.attribute.RemoveType"
+FIRST_CLASSIFIER="weka.classifiers.trees.RandomForest -- -K 10 -I 200"
+FIRST_OUTPUT=results/second-initial.txt
+
+# run first classifier on the whole dataset
+#java -cp $WEKA_LIBS weka.classifiers.meta.FilteredClassifier -F $FIRST_FILTERS -t $FIRST_ARFF -T $FIRST_ARFF -p 1 -W $FIRST_CLASSIFIER > $FIRST_OUTPUT
+
+# create an ARFF file for each class
+python second-classification/separate_arff_by_class.py "$FIRST_OUTPUT"
