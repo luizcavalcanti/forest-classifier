78223e9e4ad541dede41205b0facaac1d04905d8
Luiz Carlos Cavalcanti <cavalcanti.luiz@gmail.com>
2016-01-21 23:21:30 -0400

    [second] generate dat files with IDs per class
    Signed-off-by: Luiz Carlos Cavalcanti <cavalcanti.luiz@gmail.com>

--- second-classification/separate_arff_by_class.py
+++ second-classification/separate_arff_by_class.py
@@ -1,35 +1,23 @@
 import os
 import sys
 
-arff_path = sys.argv[1]
+results_path = sys.argv[1]
 
-arff_file = open(arff_path, 'r')
-content = arff_file.read()
-arff_lines = content.split('\n')
+results_file = open(results_path, 'r')
+content = results_file.read()
+lines = content.split('\n')[5:-2] #remove stupid empty or header lines
 
-header = []
 data = {}
+for line in lines:
+    cols = line.split()
+    classname = cols[2].split(':')[1]
+    id = cols[-1].replace('(', '').replace(')', '')
+    if classname not in data.keys():
+        data[classname] = []
+    data[classname].append(id)
 
-# getting header
-i = 0
-while arff_lines[i] != "@DATA":
-    i += 1
-i += 1
-header = arff_lines[0:i]
-
-# getting content
-for line in arff_lines[i:]:
-    params = line.split(',')
-    sample_class = params[-1]
-    if sample_class not in data.keys():
-        data[sample_class] = []
-    data[sample_class].append(line)
-
-for sample_class in data.keys():
-    if sample_class != '?':
-        lines = data[sample_class]
-        output = open('second-%s.arff' % sample_class, 'w')
-        for line in header:
-            output.write(line + '\n')
-        for line in lines:
-            output.write(line + '\n')
+for key in data.keys():
+    output = open('second-%s.dat' % key, 'w')
+    for entry in data[key]:
+        output.write('%s\n' % entry)
+    output.close()
