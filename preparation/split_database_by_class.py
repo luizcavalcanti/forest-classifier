ada7a7b8e9c68424fd849a5cdb2cba0b172dbfa6
Luiz Carlos Cavalcanti <cavalcanti.luiz@gmail.com>
2016-01-30 14:23:36 -0400

    [ensemble] fixi database split
    Signed-off-by: Luiz Carlos Cavalcanti <cavalcanti.luiz@gmail.com>

--- preparation/split_database_by_class.py
+++ preparation/split_database_by_class.py
@@ -10,8 +10,8 @@ lines = arff_file.read().split('\n')
 curr_line = 0
 classes_declaration = 0
 
-while (lines[curr_line]!='@data'):
-    if lines[curr_line][:16] == '@attribute class':
+while (lines[curr_line].upper() != '@DATA'):
+    if lines[curr_line][:16].upper() == '@ATTRIBUTE CLASS':
         classes_declaration = curr_line
     curr_line += 1
 curr_line += 1
@@ -20,24 +20,23 @@ header = lines[0:curr_line]
 # replace classes declaration for a unary class
 header[classes_declaration] = '@ATTRIBUTE class {target,outlier}'
 
+classes = ['forest', 'water', 'grass', 'dirty', 'human-made']
 data = {}
-for line in lines[curr_line:]:
-    classname = line.split(',')[-1]
-    if len(line) < 1 or classname == '?':
-        continue
-    if classname not in data.keys():
-        data[classname] = []
-    if classname == 'human-made':
-        new_line = line.replace(classname, 'outlier')
-    else:
-        new_line = line.replace(classname, 'target')
-    data[classname].append(new_line)
+
+for cls in classes:
+    data[cls] = []
+    for line in lines[curr_line:]:
+        if len(line) == 0:
+            continue
+        new_line = line.replace(cls, 'target')
+        if 'target' not in new_line:
+            other_class = new_line.split(',')[-1]
+            new_line = new_line.replace(other_class, 'outlier')
+        data[cls].append(new_line)
 
 for key in data.keys():
     output_file = open('%s-%s.arff' % (basename, key), 'w')
     for line in header:
         output_file.write('%s\n' % line)
     for line in data[key]:
-        output_file.write('%s\n' % line)
-    for line in data['human-made']:
         output_file.write('%s\n' % line)
