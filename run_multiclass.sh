cb60bb4462355175eecabc4477a5364de5c854af
Luiz Carlos Cavalcanti <cavalcanti.luiz@gmail.com>
2016-02-02 02:35:16 -0400

    fixes to support new weka version
    Signed-off-by: Luiz Carlos Cavalcanti <cavalcanti.luiz@gmail.com>

--- run_multiclass.sh
+++ run_multiclass.sh
@@ -27,28 +27,28 @@ java -cp $WEKA_LIBS weka.core.Instances "$FILTERED_ARFF"  > $RESULTS_DIR/analysi
 java -cp $WEKA_LIBS weka.core.Instances "$CFS_ARFF"  > $RESULTS_DIR/analysis-dataset-cfs.txt
 
 echo "running decision tree (J48)"
-java -cp $WEKA_LIBS weka.classifiers.trees.J48 $ARFF_OPTIONS -C 0.3 -i -d $RESULTS_DIR/model-j48.model > $RESULTS_DIR/result-j48.txt
-java -cp $WEKA_LIBS weka.classifiers.trees.J48 $ARFF_OPTIONS_CFS -C 0.3 -i -d $RESULTS_DIR/model-j48-cfs.model > $RESULTS_DIR/result-j48-cfs.txt
+java -cp $WEKA_LIBS weka.classifiers.trees.J48 $ARFF_OPTIONS -C 0.3 -k -d $RESULTS_DIR/model-j48.model > $RESULTS_DIR/result-j48.txt
+java -cp $WEKA_LIBS weka.classifiers.trees.J48 $ARFF_OPTIONS_CFS -C 0.3 -k -d $RESULTS_DIR/model-j48-cfs.model > $RESULTS_DIR/result-j48-cfs.txt
 
 echo "running random forest"
-java -cp $WEKA_LIBS weka.classifiers.trees.RandomForest $ARFF_OPTIONS -K 10 -I 200 -i -d $RESULTS_DIR/model-randomforest.model > $RESULTS_DIR/result-randomforest.txt
-java -cp $WEKA_LIBS weka.classifiers.trees.RandomForest $ARFF_OPTIONS_CFS -K 10 -I 200 -i -d $RESULTS_DIR/model-randomforest-cfs.model > $RESULTS_DIR/result-randomforest-cfs.txt
+java -cp $WEKA_LIBS weka.classifiers.trees.RandomForest $ARFF_OPTIONS -K 10 -I 200 -k -d $RESULTS_DIR/model-randomforest.model > $RESULTS_DIR/result-randomforest.txt
+java -cp $WEKA_LIBS weka.classifiers.trees.RandomForest $ARFF_OPTIONS_CFS -K 10 -I 200 -k -d $RESULTS_DIR/model-randomforest-cfs.model > $RESULTS_DIR/result-randomforest-cfs.txt
 
 echo "running kNN (IBk)"
-java -cp $WEKA_LIBS weka.classifiers.lazy.IBk $ARFF_OPTIONS -K 1 -i -d $RESULTS_DIR/model-knn1.model > $RESULTS_DIR/result-knn1.txt
-java -cp $WEKA_LIBS weka.classifiers.lazy.IBk $ARFF_OPTIONS -K 2 -i -d $RESULTS_DIR/model-knn2.model > $RESULTS_DIR/result-knn2.txt
-java -cp $WEKA_LIBS weka.classifiers.lazy.IBk $ARFF_OPTIONS -K 3 -i -d $RESULTS_DIR/model-knn3.model > $RESULTS_DIR/result-knn3.txt
-java -cp $WEKA_LIBS weka.classifiers.lazy.IBk $ARFF_OPTIONS_CFS -K 1 -i -d $RESULTS_DIR/model-knn1-cfs.model > $RESULTS_DIR/result-knn1-cfs.txt
-java -cp $WEKA_LIBS weka.classifiers.lazy.IBk $ARFF_OPTIONS_CFS -K 2 -i -d $RESULTS_DIR/model-knn2-cfs.model > $RESULTS_DIR/result-knn2-cfs.txt
-java -cp $WEKA_LIBS weka.classifiers.lazy.IBk $ARFF_OPTIONS_CFS -K 3 -i -d $RESULTS_DIR/model-knn3-cfs.model > $RESULTS_DIR/result-knn3-cfs.txt
+java -cp $WEKA_LIBS weka.classifiers.lazy.IBk $ARFF_OPTIONS -K 1 -k -d $RESULTS_DIR/model-knn1.model > $RESULTS_DIR/result-knn1.txt
+java -cp $WEKA_LIBS weka.classifiers.lazy.IBk $ARFF_OPTIONS -K 2 -k -d $RESULTS_DIR/model-knn2.model > $RESULTS_DIR/result-knn2.txt
+java -cp $WEKA_LIBS weka.classifiers.lazy.IBk $ARFF_OPTIONS -K 3 -k -d $RESULTS_DIR/model-knn3.model > $RESULTS_DIR/result-knn3.txt
+java -cp $WEKA_LIBS weka.classifiers.lazy.IBk $ARFF_OPTIONS_CFS -K 1 -k -d $RESULTS_DIR/model-knn1-cfs.model > $RESULTS_DIR/result-knn1-cfs.txt
+java -cp $WEKA_LIBS weka.classifiers.lazy.IBk $ARFF_OPTIONS_CFS -K 2 -k -d $RESULTS_DIR/model-knn2-cfs.model > $RESULTS_DIR/result-knn2-cfs.txt
+java -cp $WEKA_LIBS weka.classifiers.lazy.IBk $ARFF_OPTIONS_CFS -K 3 -k -d $RESULTS_DIR/model-knn3-cfs.model > $RESULTS_DIR/result-knn3-cfs.txt
 
 echo "running SVM"
-java -cp $WEKA_LIBS weka.classifiers.functions.LibSVM $ARFF_OPTIONS -i -d $RESULTS_DIR/model-svm.model > $RESULTS_DIR/result-svm.txt
-java -cp $WEKA_LIBS weka.classifiers.functions.LibSVM $ARFF_OPTIONS_CFS -i -d $RESULTS_DIR/model-svm-cfs.model > $RESULTS_DIR/result-svm-cfs.txt
+java -cp $WEKA_LIBS weka.classifiers.functions.LibSVM $ARFF_OPTIONS -k -d $RESULTS_DIR/model-svm.model > $RESULTS_DIR/result-svm.txt
+java -cp $WEKA_LIBS weka.classifiers.functions.LibSVM $ARFF_OPTIONS_CFS -k -d $RESULTS_DIR/model-svm-cfs.model > $RESULTS_DIR/result-svm-cfs.txt
 
 echo "running naive bayes"
-java -cp $WEKA_LIBS weka.classifiers.bayes.NaiveBayes $ARFF_OPTIONS -i -d $RESULTS_DIR/model-naive.model > $RESULTS_DIR/result-naive.txt
-java -cp $WEKA_LIBS weka.classifiers.bayes.NaiveBayes $ARFF_OPTIONS_CFS -i -d $RESULTS_DIR/model-naive-cfs.model > $RESULTS_DIR/result-naive-cfs.txt
+java -cp $WEKA_LIBS weka.classifiers.bayes.NaiveBayes $ARFF_OPTIONS -k -d $RESULTS_DIR/model-naive.model > $RESULTS_DIR/result-naive.txt
+java -cp $WEKA_LIBS weka.classifiers.bayes.NaiveBayes $ARFF_OPTIONS_CFS -k -d $RESULTS_DIR/model-naive-cfs.model > $RESULTS_DIR/result-naive-cfs.txt
 
 echo "removing temporary datasets"
 rm "$FILTERED_ARFF" 2> /dev/null
