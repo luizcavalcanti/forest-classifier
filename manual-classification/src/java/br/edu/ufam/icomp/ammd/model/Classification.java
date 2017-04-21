b879e7998d71ab1f48fd14b2cc65e1d85e848565
Luiz Carlos Cavalcanti <cavalcanti.luiz@gmail.com>
2017-04-21 17:34:31 -0300

    Cleanup old java code for manual classification
    Signed-off-by: Luiz Carlos Cavalcanti <cavalcanti.luiz@gmail.com>

--- manual-classification/src/java/br/edu/ufam/icomp/ammd/model/Classification.java
+++
@@ -1,9 +0,0 @@
-package br.edu.ufam.icomp.ammd.model;
-
-public class Classification {
-    public static final char NONE = '0';
-    public static final char ROAD = 'r';
-    public static final char FOREST = 'f';
-    public static final char WATER = 'w';
-    public static final char BUILDING = 'b';
-}
